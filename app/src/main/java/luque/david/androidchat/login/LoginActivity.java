955ad157b2e66ba5bcf6b9d350072e6e8c0088e1
David Luque <daviluqui@gmail.com>
2016-09-14 12:05:48 +0200

    Implementing view and presenter

--- app/src/main/java/luque/david/androidchat/login/LoginActivity.java
+++ app/src/main/java/luque/david/androidchat/login/LoginActivity.java
@@ -1,22 +1,43 @@
 package luque.david.androidchat.login;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import luque.david.androidchat.R;
+import luque.david.androidchat.contactlist.ContactListActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginView{
 
     @Bind(R.id.editTxtEmail)
     EditText inputEmail;
 
     @Bind(R.id.editTxtPassword)
     EditText inputPassword;
+
+    @Bind(R.id.btnSinging)
+    Button btnSinging;
+
+    @Bind(R.id.btnSingup)
+    Button btnSingup;
+
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+
+    @Bind(R.id.layoutMainContainer)
+    RelativeLayout container;
+
+    private LoginPresenter loginPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +54,68 @@ public class LoginActivity extends AppCompatActivity {
     @OnClick(R.id.btnSingup)
     public void handleSingup(){
 
+    }
+
+    @Override
+    public void enableInputs() {
+        setInputs(true);
+    }
+
+    @Override
+    public void disableInputs() {
+        setInputs(false);
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.btnSingup)
+    @Override
+    public void handleSignUp() {
+        loginPresenter.registerNewUser(inputEmail.getText().toString(), inputPassword.getText().toString());
+    }
+
+    @OnClick(R.id.btnSinging)
+    @Override
+    public void handleSignIn() {
+        loginPresenter.validateLogin(inputEmail.getText().toString(), inputPassword.getText().toString());
+    }
+
+    @Override
+    public void navigateToMainScreen() {
+        startActivity(new Intent(this, ContactListActivity.class));
+    }
+
+    @Override
+    public void loginError(String error) {
+        inputPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signing), error);
+        inputPassword.setError(msgError);
+    }
+
+    @Override
+    public void newUserSuccess() {
+        Snackbar.make(container, R.string.login_notice_message_signup, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void newUserError(String error) {
+        inputPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signup), error);
+        inputPassword.setError(msgError);
+    }
+
+    private void setInputs(boolean enabled){
+        inputEmail.setEnabled(enabled);
+        inputPassword.setEnabled(enabled);
+        btnSinging.setEnabled(enabled);
+        btnSingup.setEnabled(enabled);
     }
 }
---
+++ app/src/main/java/luque/david/androidchat/login/LoginPresenterImpl.java
@@ -0,0 +1,7 @@
+package luque.david.androidchat.login;
+
+/**
+ * Created by David on 14/9/16.
+ */
+public class LoginPresenterImpl {
+}
