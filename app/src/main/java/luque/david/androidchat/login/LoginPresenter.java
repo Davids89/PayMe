dce66210fc0342c337d4c51470df2ef2c044744d
David Luque <daviluqui@gmail.com>
2016-09-14 10:53:00 +0200

    Login presenter

--- app/src/main/java/luque/david/androidchat/login/LoginPresenter.java
+++ app/src/main/java/luque/david/androidchat/login/LoginPresenter.java
@@ -4,4 +4,8 @@ package luque.david.androidchat.login;
  * Created by David on 14/9/16.
  */
 public interface LoginPresenter {
+    void onDestroy();
+    void checkForAuthenticatedUser();
+    void validateLogin(String email, String password);
+    void registerNewUser(String email, String password);
 }
