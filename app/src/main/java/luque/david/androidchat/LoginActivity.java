cfae389644a08a07312817910383230ab5083141
David Luque <daviluqui@gmail.com>
2016-09-14 10:09:31 +0200

    Finishing Firebase Class

--- app/src/main/java/luque/david/androidchat/LoginActivity.java
+++ app/src/main/java/luque/david/androidchat/LoginActivity.java
@@ -2,12 +2,35 @@ package luque.david.androidchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.editTxtEmail)
+    EditText inputEmail;
+
+    @Bind(R.id.editTxtPassword)
+    EditText inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSinging)
+    public void handleSinging(){
+        Log.e("AndroidChat", inputEmail.getText().toString());
+    }
+
+    @OnClick(R.id.btnSingup)
+    public void handleSingup(){
+
     }
 }
