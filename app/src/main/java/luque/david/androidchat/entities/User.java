d8fc30b404d760e6569cd287262cb1ab45ecfc8c
David Luque <daviluqui@gmail.com>
2016-09-19 22:34:34 +0200

    Login and signup created

---
+++ app/src/main/java/luque/david/androidchat/entities/User.java
@@ -0,0 +1,43 @@
+package luque.david.androidchat.entities;
+
+import java.util.Map;
+
+/**
+ * Created by David on 19/9/16.
+ */
+public class User {
+    String email;
+    boolean online;
+    Map<String, Boolean> contacts;
+    public final static boolean ONLINE = true;
+
+    public Map<String, Boolean> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(Map<String, Boolean> contacts) {
+        this.contacts = contacts;
+    }
+
+    public boolean isOnline() {
+        return online;
+    }
+
+    public void setOnline(boolean online) {
+        this.online = online;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public final static boolean OFFLINE = false;
+
+    public User() {
+
+    }
+}
