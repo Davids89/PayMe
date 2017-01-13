38d6e272a04fe84ae4b01ba3c5ca620761edfbe5
Davids89 <daviluqui@gmail.com>
2017-01-12 18:27:18 -0800

    Changing id for the Firebase data id

--- app/src/main/java/luque/david/androidchat/payments/PaymentRepositoryImpl.java
+++ app/src/main/java/luque/david/androidchat/payments/PaymentRepositoryImpl.java
@@ -3,9 +3,7 @@ package luque.david.androidchat.payments;
 
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
 
 import luque.david.androidchat.domain.FirebaseHelper;
 import luque.david.androidchat.entities.Deal;
@@ -69,6 +67,7 @@ public class PaymentRepositoryImpl implements PaymentRepository {
 
         for(DataSnapshot data : dataSnapshot.getChildren()){
             final Deal deal = data.getValue(Deal.class);
+            deal.setDealId(data.getKey());
             if(deal.getContacts() != null){
                 Boolean isMine = deal.getContacts().get(userEmail);
                 if(isMine != null){
